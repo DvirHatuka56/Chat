a3d43f5ad6b9eceb056c9602fd0ec94db680dad6
DvirHatuka56 <DvirHatuka@gmail.com>
2020-07-20 02:48:20 +0300

    Implemented Login sequence

--- ConsoleChatClient/ChatClient.cs
+++ ConsoleChatClient/ChatClient.cs
@@ -1,5 +1,7 @@
 ﻿using ChatServer;
 using ConsoleChatClient.Models;
+using ConsoleChatClient.Requests;
+using ConsoleChatClient.Responses;
 using Network.Client;
 
 namespace ConsoleChatClient
@@ -20,7 +22,32 @@ namespace ConsoleChatClient
             Client = new Client(Constants.IP, Constants.PORT);
             User = new User();
         }
-        
-        
+
+        public bool Login(int id, string password, out ErrorResponse error)
+        {
+            Client.SendMessage(new LoginRequest(id, password).ToString());
+            string rawData = ReceiveResponse();
+            if (rawData != "200")
+            {
+                error = new ErrorResponse();
+                error.Deserialize(rawData);
+                return false;
+            }
+            
+            LoginResponse response = new LoginResponse();
+            response.Deserialize(rawData);
+
+            User.Id = response.Id;
+            User.Name = response.Name;
+
+            error = null;
+            return true;
+        }
+
+        private string ReceiveResponse()
+        {
+            int len = Client.ReceiveInt(Constants.ID_SEGMNET);
+            return Client.ReceiveString(len);
+        }
     }
 }
---
+++ ConsoleChatClient/Responses/LoginResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ChatServer;
+
+namespace ConsoleChatClient.Responses
+{
+    public class LoginResponse : Response
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public LoginResponse()
+        {
+            Code = ResponseCode.Success;
+        }
+
+        public void Deserialize(string response)
+        {
+            if (!response.Substring(0, Constants.CODE_SEGMNET).Equals("200")) { throw new FormatException(); }
+
+            int i = Constants.CODE_SEGMNET;
+            Id = int.Parse(response.Substring(i, Constants.ID_SEGMNET));
+
+            i += Constants.ID_SEGMNET;
+            Name = response.Substring(i);
+        }
+    }
+}
